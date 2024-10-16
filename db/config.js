69a31efaae26decd5b407878e718532626ce3302
ritik kumar <ritikkumarkashyap9@gmail.com>
2024-10-16 23:20:40 +0530

    setup for deployment

--- db/config.js
+++ db/config.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const winston = require('winston');
+const winston = require("winston");
 
 async function dbConnection() {
   await mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.PROD_URI)
     .then(() => {
       console.log("Vidly database connected succesfully");
     })
